8a11771fcd761c40eae6cd76f5f2c1270b0620a1
ili-che-w <john.the.theologician@gmail.com>
2021-03-11 17:29:20 +0300

    mid-day endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IPost {
+  backdrop_path: string | null
+  id: number
+  overview: string
+  title: string
+  poster_path: string | null
+  release_date: string
 }
 
-export default App;
+interface IResult {
+  results: IPost[]
+}
+
+const defaultPosts: IPost[] = []
+
+const App: React.SFC = () => {
+  const url =
+    'https://api.themoviedb.org/3/discover/movie?api_key=5a04ce8778f4b2fcf7a03d527e0ac099&language=en-US&sort_by=popularity.asc&include_adult=false&include_video=false&page=1'
+  const headers = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(
+    defaultPosts
+  )
+
+  const [loading, setLoading]: [
+    boolean,
+    (loading: boolean) => void
+  ] = React.useState<boolean>(true)
+
+  const [error, setError]: [string, (error: string) => void] = React.useState(
+    ''
+  )
+
+  React.useEffect(() => {
+    axios
+      .get<IResult>(url, headers)
+      .then((response) => {
+        setPosts(response.data.results)
+        setLoading(false)
+      })
+      .catch((ex) => {
+        const err =
+          ex.response.status === 404
+            ? 'Resource not found'
+            : 'An unexpected error occured'
+        setError(err)
+        setLoading(false)
+      })
+  })
+
+  return (
+    <div className="App container">
+      <div className="posts">
+        {posts.map((post) => (
+          <div className="row" key={post.id}>
+            <div className="col-3 image">
+              {post.poster_path ? <pre>{post.poster_path}</pre> : <p>No image</p>}
+            </div>
+            <div className="col-9">
+              <h3>{post.title}</h3>
+              <p>{post.overview}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      {error && <p className="error">{error}</p>}
+    </div>
+  )
+}
+
+export default App
