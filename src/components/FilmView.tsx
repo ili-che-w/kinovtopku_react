606eb4daf9c4e4022639579edc6d5ccb7521fefe
ili-che-w <john.the.theologician@gmail.com>
2021-03-12 18:28:14 +0300

    fixed image display

--- src/components/FilmView.tsx
+++ src/components/FilmView.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
+import Config from '../config'
 import { IFilm } from '../film.model'
 
 interface IProps {
@@ -13,13 +14,20 @@ interface RouteParams {
 
 const FilmView: React.SFC<IProps> = (props) => {
   const { id } = useParams<RouteParams>()
-  const currentFilm = props.films.find(film => film.id.toString() === id)
+  const currentFilm = props.films.find((film) => film.id.toString() === id)
 
   return (
     <div className="film-card">
       <div className="row">
         <div className="col-md-3 film-card__poster">
-          <pre>{currentFilm?.poster_path || 'No poster'}</pre>
+          {currentFilm?.poster_path ? (
+            <img
+              src={Config.imgBaseUrl + currentFilm.poster_path}
+              alt="Film poster"
+            />
+          ) : (
+            <span>No image</span>
+          )}
         </div>
         <div className="col-md-9 film-card__details">
           <h2 className="film-card__title">{currentFilm?.title}</h2>
